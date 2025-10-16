13738e64c89dcd8dceb0bac9ac2bd1e203918bfd
Ilham Seferov <snin_emailin@example.com>
2025-10-16 11:03:41 +0400

    some chnages made

--- config/settings.py
+++ config/settings.py
@@ -20,17 +20,12 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-this')
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    'https://eccomerce-analitica-808729853617.europe-west1.run.app',
+    '*'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "localhost",
-    "127.0.0.1",
-    "https://eccomerce-analitica-808729853617.europe-west1.run.app",
+   '*'
 ]
-
 
 # --- Applications ---
 INSTALLED_APPS = [
