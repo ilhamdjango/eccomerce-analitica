5f207efe2233367f79db05e13a10fa02b5141951
Ilham Safarov <seferov.ilham@internet.ru>
2025-10-16 14:04:01 +0400

    Update Django project

--- config/settings.py
+++ config/settings.py
@@ -24,16 +24,16 @@ ALLOWED_HOSTS = [
 
 
 # serverde
-# CSRF_TRUSTED_ORIGINS = [
-#         '*'
-# ]
-
-
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000',       # Local server
-    'http://localhost',             # Local test
-    'https://<cloud-run-domain>'    # Cloud Run domain
+        '*'
 ]
+
+
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://127.0.0.1:8000',       # Local server
+#     'http://localhost',             # Local test
+#     'https://<cloud-run-domain>'    # Cloud Run domain
+# ]
 
 # --- Applications ---
 INSTALLED_APPS = [
