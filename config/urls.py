a896bafa03cc432f33bc95b53398a1d5090a9c0a
Ilham Safarov <seferov.ilham@internet.ru>
2025-10-20 19:50:43 +0400

    OpenAPI JSON

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     # API v2
     path('api/v2/', include('analitic.urls')),
 
+    # OpenAPI JSON
+    path('openapi.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
     # Swagger / Redoc
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
